457899ecf421648c624262bae819d3ea8929d855
Alvaro Cardozo <alvaro_car6@hotmail.com>
2022-02-10 12:25:11 -0500

    version 1.0

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const userService = require('./service/user')
 const app = express()
 const port = 3000
 
+app.set('views', `${__dirname}/views`)
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
   userService.getAll()
   .then((result) => {
-    res.json(result)
+    res.status(200)
+    .render('index.pug', {
+      users: result
+    })
   })
 })
 
